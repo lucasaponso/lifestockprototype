4235dd6a3698ee81bd3e7d28677adbe8711735d0
Lucas Aponso <lucasaponso@outlook.com>
2022-06-01 15:51:13 +1000

    first commit

---
+++ dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_USER', "id19021827_lifestockdbuser");     
+define('DB_PASSWORD', "Entertainment*2");			
+define('DB_DATABASE', "id19021827_lifestockdb"); 
+define('DB_SERVER', "localhost");			
+?>
